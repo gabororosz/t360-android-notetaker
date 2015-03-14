b7f481d1ddd49f3886884a01ae226ba6fc90c792
Gabor Orosz <gabor.orosz@me.com>
2015-03-15 00:47:41 +0100

    Populate dummy data into noteListView

--- app/src/main/java/com/go/notetaker/NoteListActivity.java
+++ app/src/main/java/com/go/notetaker/NoteListActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class NoteListActivity extends ActionBarActivity {
@@ -15,6 +19,21 @@ public class NoteListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_note_list);
 
         final ListView noteList = (ListView) findViewById(R.id.noteListView);
+
+        // Létre kell hozni Stringek egy listáját, amiben az egyes jegyzetek címei találhatóak,
+        // ezek még csak kamu elemek.
+        List<String> values = new ArrayList<>();
+        values.add("First note");
+        values.add("Second note");
+        values.add("Third note");
+        values.add("Fourth note");
+        values.add("Fifth note");
+
+        // Szükségünk van a ListView-hoz egy adapterre, kezdésnek használjuk ezt az egyszerű megoldást,
+        // az android.R. azt jelenti be van építve az Android rendszerbe, de az is sima resource,
+        // control + click akár a simple_list_item_1-re, akár a text1-re, és jó lesz.
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        noteList.setAdapter(adapter);
     }
 
 
